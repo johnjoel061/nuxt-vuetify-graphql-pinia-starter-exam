e94be860ac652015382405e2041cff85847f94fe
johnjoel061 <alfabetejohnjoel@gmail.com>
2025-06-26 17:14:43 +0800

    middleware that redirects users to the launch page if they try to access a page that does not exist.

--- app/pages/[...all].vue
+++ app/pages/[...all].vue
@@ -1,9 +1,12 @@
 <!-- pages/[...all].vue -->
+ 
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 
+
 const router = useRouter()
+
 
 onMounted(() => {
 	router.replace('./launches')
